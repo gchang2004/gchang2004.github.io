a02cf89d2f4fc02f2d4506d45202b2b7a05fb6a0
Gavin Chang <changgavin2012@gmail.com>
2022-08-20 18:53:13 -0700

    update 1.24

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -113,6 +113,7 @@ const Footer = () => {
               <option value="en">🇺🇸 United States</option>
               <option value="de">🇩🇪 Deutschland</option>
               <option value="es">🇪🇸 España</option>
+              <option value="fr">🇫🇷 France</option>
               <option value="cn">🇨🇳 中国 (简体)</option>
               <option value="tw">🇹🇼 台灣 (繁體)</option>
             </select> 
